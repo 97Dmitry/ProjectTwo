9aee282594a5e573e0227018b4eb9a8666147de4
97Dmitry <dart_veyder_1997@mail.ru>
2020-11-20 23:43:23 +0600

    Updated model

--- bboard/views.py
+++ bboard/views.py
@@ -12,6 +12,6 @@ from django.template import loader
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
 
