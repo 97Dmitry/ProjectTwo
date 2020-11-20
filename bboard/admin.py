7b9bde6498e8332371eb629ad6cc585093e75348
97Dmitry <dart_veyder_1997@mail.ru>
2020-11-21 01:45:07 +0600

    Added model tegs for bulletin board

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Bb
+from .models import Bb, Rubric
+
 
 
 # Register your models here.
 
-admin.site.register(Bb)
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published') # Информация, которая будет показана на админ сайте
+    list_display_links = ('title', 'content') # Поля, которые будут ссылками на обьект, на админ сайте
+    search_fields = ('title', 'content') # Поля, по которым будет вестись поиск на админ сайте
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
--- bboard/models.py
+++ bboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Bb(models.Model):
@@ -7,9 +8,18 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публицкации')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Тег')
 
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published']
 
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=25, db_index=True, verbose_name='Тег')
+
+    class Meta:
+        verbose_name_plural = 'Теги'
+        verbose_name = 'Тег'
+        ordering = ['name']
