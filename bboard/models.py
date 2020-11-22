381ea19d368a8e3e852f47c63fd3444459989187
97Dmitry <dart_veyder_1997@mail.ru>
2020-11-23 03:10:36 +0600

    Added choice kind ad

--- bboard/models.py
+++ bboard/models.py
@@ -4,11 +4,18 @@ from django.db import models
 # Create your models here.
 
 class Bb(models.Model):
+    KINDS = (
+        (None, 'Выберите тип публикации'),
+        ('b', 'Куплю'),
+        ('s', 'Продам'),
+        ('c', 'Обменяю')
+    )
     title = models.CharField(max_length=50, verbose_name='Товар или услуга')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публицкации')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Тег')
+    #kind = models.CharField(max_length=1, choices=KINDS) # TODO
 
     class Meta:
         verbose_name_plural = 'Объявления'
